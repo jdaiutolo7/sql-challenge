81f9e79f99bb2924679fa188e5adb5d5b1928f9a
jdaiutolo7 <127791011+jdaiutolo7@users.noreply.github.com>
2023-05-19 03:59:24 -0400

    Update sql_data_engineering.sql

--- sql/sql_data_engineering.sql
+++ sql/sql_data_engineering.sql
@@ -42,7 +42,7 @@ CREATE TABLE dept_emp (
 
 CREATE TABLE dept_manager (
     dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
+    emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no)
 	REFERENCES employees (emp_no),
 	PRIMARY KEY (dept_no, emp_no)
