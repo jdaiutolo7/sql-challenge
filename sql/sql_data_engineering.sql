a67e2344bbf9937aa01ad2501763263e5268aa63
jdaiutolo7 <127791011+jdaiutolo7@users.noreply.github.com>
2023-05-19 03:57:02 -0400

    Update sql_data_engineering.sql

--- sql/sql_data_engineering.sql
+++ sql/sql_data_engineering.sql
@@ -6,33 +6,33 @@ DROP TABLE IF EXISTS salaries CASCADE;
 DROP TABLE IF EXISTS titles CASCADE;
 
 CREATE TABLE departments (
-    dept_no VARCHAR NOT NULL,
-    dept_name VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE titles (
-    title_id VARCHAR NOT NULL,
-    title VARCHAR NOT NULL,
+    title_id VARCHAR(5) NOT NULL,
+    title VARCHAR(30) NOT NULL,
 	PRIMARY KEY (title_id)
 );
 
 CREATE TABLE employees (
-    emp_no INT,
-    emp_title_id VARCHAR NOT NULL,
-    birth_date VARCHAR NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    sex VARCHAR NOT NULL,
-    hire_date VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR(5) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    sex VARCHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_title_id)
 	REFERENCES titles (title_id)
 );
 
 CREATE TABLE dept_emp (
-    emp_no INT,
-    dept_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
 	FOREIGN KEY (emp_no)
 	REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no)
@@ -41,16 +41,16 @@ CREATE TABLE dept_emp (
 );
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR NOT NULL,
-	emp_no INT,
+    dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no)
 	REFERENCES employees (emp_no),
 	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE salaries (
-    emp_no INT,
-    salary INT,
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_no)
 	REFERENCES employees (emp_no)
